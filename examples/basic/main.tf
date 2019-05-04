383720d90f970665ddcce77764e7f6dc3a11b4c3
jrbeverly <jrbeverly@live.com>
2019-05-04 12:50:35 -0400

    Simplify the example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,18 +2,10 @@ module "example" {
   source = "../../"
 
   user  = "${aws_iam_user.user.name}"
-  phase = "${var.phase}"
+  phase = 0
 }
 
 resource "aws_iam_user" "user" {
   name = "infraprints-user"
   path = "/system/"
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
-
-variable "phase" {
-  default = 1
 }
